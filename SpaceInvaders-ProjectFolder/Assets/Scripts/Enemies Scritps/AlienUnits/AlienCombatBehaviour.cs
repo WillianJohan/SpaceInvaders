7511ee671707940b572416fbf9f67af170b0b766
Willian Johan <johan_willian@hotmail.com>
2021-06-07 17:24:56 -0300

    Alien atirando no jogador (Implementação iniciada)

--- SpaceInvaders-ProjectFolder/Assets/Scripts/Enemies Scritps/AlienUnits/AlienCombatBehaviour.cs
+++ SpaceInvaders-ProjectFolder/Assets/Scripts/Enemies Scritps/AlienUnits/AlienCombatBehaviour.cs
@@ -4,8 +4,44 @@ using UnityEngine;
 
 public class AlienCombatBehaviour : MonoBehaviour
 {
+    [Header("Layers")]
     [SerializeField] LayerMask BarrierLayer;
     [SerializeField] LayerMask PlayerLayer;
+    [SerializeField] LayerMask AlienLayer;
+
+    [Header("Shoot variables")]
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] Transform projectileInitialPosition;
+    float checkPlayerPosition_DelayTime;
+    float lastCheck = 0;
+
+    private void Start()
+    {
+        checkPlayerPosition_DelayTime = Random.Range(3, 8);
+    }
+
+    void Update()
+    {
+        //Check if the player is below 
+        //Raycast down
+
+        lastCheck += Time.deltaTime;
+
+        if (lastCheck < checkPlayerPosition_DelayTime)
+            return;
+
+        if(!Physics.Raycast(projectileInitialPosition.position, -Vector3.up, out RaycastHit hitInfo))
+            return;
+
+        if (((AlienLayer & 1 << hitInfo.transform.gameObject.layer) == (1 << hitInfo.transform.gameObject.layer)))
+            return;
+
+        Instantiate(projectilePrefab, projectileInitialPosition.position, Quaternion.identity);
+
+        lastCheck = 0;
+
+    }
+
 
     void TryKillPlayer(Collider other)
     {
