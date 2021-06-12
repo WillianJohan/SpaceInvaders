76e36f64882b4ad3c6c856b5dd0253fa93813200
Willian Johan <johan_willian@hotmail.com>
2021-06-12 04:30:42 -0300

    Update AlienHealthHandler.cs

--- SpaceInvaders-ProjectFolder/Assets/Scripts/Enemies Scritps/AlienUnits/AlienHealthHandler.cs
+++ SpaceInvaders-ProjectFolder/Assets/Scripts/Enemies Scritps/AlienUnits/AlienHealthHandler.cs
@@ -7,16 +7,23 @@ public class AlienHealthHandler : MonoBehaviour
 {
     
     [SerializeField] Health health;
+    AlienType type;
 
     public static event Action<AlienType> OnAlienDie;
+    public static event Action<AlienType> OnAlienSpawn;
 
-    void Awake() => health.OnDie += HandleDie;
-    void OnDestroy() => health.OnDie -= HandleDie;
+    void Awake()        => health.OnDie += HandleDie;
+    void OnDestroy()    => health.OnDie -= HandleDie;
 
+    void Start()
+    {
+        type = GetComponent<Alien>().Type;
+        OnAlienSpawn?.Invoke(type);
+    }
 
     void HandleDie()
     {
-        OnAlienDie?.Invoke(GetComponent<Alien>().Type);
+        OnAlienDie?.Invoke(type);
         Destroy(gameObject);
     }
 
