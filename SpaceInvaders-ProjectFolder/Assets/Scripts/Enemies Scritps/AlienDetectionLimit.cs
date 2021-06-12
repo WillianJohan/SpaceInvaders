228bc0cf9454dcf5e1676bcde74e8c3039af86bb
Willian Johan <johan_willian@hotmail.com>
2021-06-12 04:30:51 -0300

    Update AlienDetectionLimit.cs

--- SpaceInvaders-ProjectFolder/Assets/Scripts/Enemies Scritps/AlienDetectionLimit.cs
+++ SpaceInvaders-ProjectFolder/Assets/Scripts/Enemies Scritps/AlienDetectionLimit.cs
@@ -6,8 +6,16 @@ public class AlienDetectionLimit : MonoBehaviour
     [SerializeField] GameObject OtherBarrier;
     [SerializeField] LayerMask DetectionLayer;
 
+
     public static event Action AlienReachedTheLimit;
 
+
+    void Awake()        => GameManager.StartGame += HandleStartGame;
+
+    void OnDestroy()    => GameManager.StartGame -= HandleStartGame;
+
+
+    void HandleStartGame() => OtherBarrier.SetActive(true);
     void OnTriggerEnter(Collider other)
     {
         if ((DetectionLayer & 1 << other.gameObject.layer) != (1 << other.gameObject.layer))
