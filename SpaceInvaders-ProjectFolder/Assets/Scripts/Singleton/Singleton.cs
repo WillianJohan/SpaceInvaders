fd687b52e98c469d09bfe477fe4ce3de43ab8f80
Willian Johan <johan_willian@hotmail.com>
2022-01-05 17:20:17 -0300

    Update Singleton.cs

--- SpaceInvaders-ProjectFolder/Assets/Scripts/Singleton/Singleton.cs
+++ SpaceInvaders-ProjectFolder/Assets/Scripts/Singleton/Singleton.cs
@@ -2,7 +2,7 @@
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    public static T Instance { get; private set; }
+    public static T Instance { get; protected set; }
     
     protected virtual void Awake()
     {
